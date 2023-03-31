081b48c82946680c2bc5c3af4a454b19bcc00b60
Filip Bušek <filip.busek@protonmail.com>
2023-03-31 21:39:36 +0200

    Přidat rust do LSP config

--- lua/filip/lsp_config.lua
+++ lua/filip/lsp_config.lua
@@ -1,19 +1,32 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-	ensure_installed = { 'lua_ls', 'ansiblels', 'bashls' }
+        ensure_installed = { 
+                'lua_ls',
+                'ansiblels',
+                'bashls',
+                'yamlls',
+                'rust_analyzer'
+        }
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('lspconfig').lua_ls.setup {
-	capabilities = capabilities
+        capabilities = capabilities
 }
 
 require('lspconfig').ansiblels.setup{
-	capabilities = capabilities
+        capabilities = capabilities
 }
 
 require('lspconfig').bashls.setup{
-	capabilities = capabilities
+        capabilities = capabilities
 }
 
+require('lspconfig').yamlls.setup{
+        capabilities = capabilities
+}
+
+require('lspconfig').rust_analyzer.setup{
+        capabilities = capabilities
+}
