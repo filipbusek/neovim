992b629c90e8c35298aa8445b2be7f141b02d837
Filip Bušek <filipbusek@Filip-MacBook-Air.local>
2023-04-07 17:58:36 +0200

    Completions can be now used with Enter and Tab

--- lua/filip/completions.lua
+++ lua/filip/completions.lua
@@ -7,10 +7,10 @@ cmp.setup({
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 --		['<C-o>'] = cmp.mapping.scroll_complete(),
 		['<C-e>'] = cmp.mapping.close(),
-		['<C-y>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		}),
+		['<C-y>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+		['<Enter>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+		['<Tab>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+	
 	}),
 
 	sources = cmp.config.sources({
